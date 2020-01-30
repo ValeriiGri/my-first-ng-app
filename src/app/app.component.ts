566cc3067f15bb1648a9b886b0d548c93ba42234
grigoryev <v.grigoryev@iac.spb.ru>
2020-01-30 21:14:26 +0300

    - second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatDrawer} from '@angular/material';
 
 @Component({
   selector: 'course-root',
@@ -7,35 +8,38 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-   public t = 'my-first-ng-app';
+    public titleApp = 'my-first-ng-app';
 
-   public account: {name: string, age: number} = {name: 'Qwerty', age: 33};
-   public salary = 3000;
+    public account: {name: string, age: number} = {name: 'Qwerty', age: 33};
+    public salary = 3000;
 
-   public getSalary(bonus: number): number {
-     return this.salary * bonus;
-   };
+    public imgWidth = 100;
+    public imgSrc = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png';
 
-   public imgWidth = 100;
+    public drawer!: MatDrawer;
 
-   public imgSrc = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png';
-
-   public  getWidth(width: number): void{
+    public getSalary(bonus: number): number {
+        return this.salary * bonus;
+    }
+    public  getWidth(width: number): void{
        console.log(width);
-   }
+    }
 
     public  getElement(el: HTMLImageElement): void{
         console.log(el);
     }
 
-    //прокидываем event
+    // прокидываем event
     public search(width: number, event: Event): void{
        const value = (event.target as HTMLInputElement).value;
        console.log(width, value);
     }
 
-    //прокидываем инпут + деструктуризация, чтобы упростить код
+    // прокидываем инпут + деструктуризация, чтобы упростить код
     public searchLink(width: number, {value}: HTMLInputElement): void{
         console.log(width, value);
+    }
+    public setSidenav(drawer: MatDrawer) {
+       this.drawer = drawer;
     }
 }
